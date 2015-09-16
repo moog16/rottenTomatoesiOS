9ba9c1a0daece749f094e35c42c19025e9e50565
Matt Goo <mattgoo@gmail.com>
2015-09-16 16:41:08 -0700

    created movie cell custom cell. getting synopsis and title on cell

---
+++ RottenTomatoes/MovieCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieCell.swift
+//  RottenTomatoes
+//
+//  Created by Matthew Goo on 9/16/15.
+//  Copyright © 2015 mattgoo. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+}
