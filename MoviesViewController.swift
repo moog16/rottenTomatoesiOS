8c2e792112be2185b70a165efb726a62edbe085d
Matt Goo <mattgoo@gmail.com>
2015-09-18 15:57:18 -0700

    added network error as a table cell

--- MoviesViewController.swift
+++ MoviesViewController.swift
@@ -16,31 +16,37 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     
     var movies: [NSDictionary]?
     var refreshControl: UIRefreshControl!
+    var hasFetchingMoviesError: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
         
-        let url = NSURL(string: "https://gist.githubusercontent.com/timothy1ee/d1778ca5b944ed974db0/raw/489d812c7ceeec0ac15ab77bf7c47849f2d1eb2b/gistfile1.json")! // bad URL
+//        let url = NSURL(string: "https://gist.githubusercontent.com/timothy1ee/d1778ca5b944ed974db0/raw/489d812c7ceeec0ac15ab77bf7c47849f2d1eb2b/gistfile1.json")! // good URL
         
-//        let url = NSURL(string: "https://gist.githubusercontent.com/timothy1ee0/d1778ca5b944ed974db0/raw/4c15ab77bf7c47849f2d1eb2b/gistfile1.json")! // good URL
+        let url = NSURL(string: "https://gist.githubusercontent.com/timothy1ee0/d1778ca5b944ed974db0/raw/4c15ab77bf7c47849f2d1eb2b/gistfile1.json")! // bad URL
 
         let request = NSURLRequest(URL: url)
         MBProgressHUD.showHUDAddedTo(self.view, animated: true)
-        let getJsonTask = NSURLSession.sharedSession().dataTaskWithRequest(request) { (data, response, error) -> Void in
+        let sessionConfiguration = NSURLSessionConfiguration.defaultSessionConfiguration()
+        let session = NSURLSession(configuration: sessionConfiguration, delegate: nil, delegateQueue: NSOperationQueue.mainQueue())
+        let jsonTask = session.dataTaskWithRequest(request) {(data, response, error) -> Void in
             do {
                 let json = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers)
                 self.movies = json["movies"] as? [NSDictionary]
                 self.tableView.reloadData()
             } catch {
+                self.hasFetchingMoviesError = true
+                self.movies = NSArray() as? [NSDictionary]
+                self.movies?.append(["error": true])
+                self.tableView.reloadData()
                 print("error: \(error)")
-                self.tableView.hidden = true
             }
             MBProgressHUD.hideHUDForView(self.view, animated: true)
         }
-        getJsonTask.resume()
-        
+
+        jsonTask.resume()
 //        refreshControl = UIRefreshControl()
 //        refreshControl.addTarget(self, action: "onRefresh", forControlEvents: UIControlEvents.ValueChanged)
 //        self.tableView.insertSubview(refreshControl, atIndex: 0)
@@ -55,18 +61,22 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     func  tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! MovieCell
-        
-        let movie = movies![indexPath.row]
-        let posterUrl = NSURL(string: movie.valueForKeyPath("posters.thumbnail") as! String)!
-        
-        cell.titleLabel.text = movie["title"] as? String
-        cell.synopsisLabel.text = movie["synopsis"] as? String
-        print(posterUrl)
-        cell.posterView.setImageWithURL(posterUrl)
-        
-        
-        return cell
+        if(movies!.count == 1 && hasFetchingMoviesError) {
+            let cell = tableView.dequeueReusableCellWithIdentifier("NetworkErrorCell", forIndexPath: indexPath) as! NetworkErrorCell
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! MovieCell
+            
+            let movie = movies![indexPath.row]
+            let posterUrl = NSURL(string: movie.valueForKeyPath("posters.thumbnail") as! String)!
+            
+            cell.titleLabel.text = movie["title"] as? String
+            cell.synopsisLabel.text = movie["synopsis"] as? String
+            print(posterUrl)
+            cell.posterView.setImageWithURL(posterUrl)
+            
+            return cell
+        }
     }
     
     func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
