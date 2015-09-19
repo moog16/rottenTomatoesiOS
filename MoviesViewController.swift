ddb0c89f23538a6755651182414f3082d906c131
Matt Goo <mattgoo@gmail.com>
2015-09-18 17:08:13 -0700

    have refreshControl working properly

--- MoviesViewController.swift
+++ MoviesViewController.swift
@@ -17,19 +17,21 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     
     var movies: [NSDictionary]?
     var refreshControl: UIRefreshControl!
+    var originTableViewOriginY: CGFloat!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
-        let url = NSURL(string: "https://gist.githubusercontent.com/timothy1ee/d1778ca5b944ed974db0/raw/489d812c7ceeec0ac15ab77bf7c47849f2d1eb2b/gistfile1.json")!
+        originTableViewOriginY = tableView.frame.origin.y
         
-//        let url = NSURL(string: "https://gist.githubusercontent.com/timothy1ee0/d1778ca5b944ed974db0/raw/4c15ab77bf7c47849f2d1eb2b/gistfile1.json")! // bad URL
+        let url = NSURL(string: "https://gist.githubusercontent.com/timothy1ee0/d1778ca5b944ed974db0/raw/4c15ab77bf7c47849f2d1eb2b/gistfile1.json")! // bad URL
+
         fetchMovies(url)
 
-//        refreshControl = UIRefreshControl()
-//        refreshControl.addTarget(self, action: "onRefresh", forControlEvents: UIControlEvents.ValueChanged)
-//        self.tableView.insertSubview(refreshControl, atIndex: 0)
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: "onRefresh", forControlEvents: UIControlEvents.ValueChanged)
+        self.tableView.insertSubview(refreshControl, atIndex: 0)
         
     }
     
@@ -69,17 +71,43 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         movieDetailsViewController.movie = movie
     }
     
+    func delay(delay:Double, closure:()->()) {
+        dispatch_after(
+            dispatch_time(
+                DISPATCH_TIME_NOW,
+                Int64(delay * Double(NSEC_PER_SEC))
+            ),
+            dispatch_get_main_queue(), closure)
+    }
+    
+    func onRefresh() -> Void {
+        let url = NSURL(string: "https://gist.githubusercontent.com/timothy1ee/d1778ca5b944ed974db0/raw/489d812c7ceeec0ac15ab77bf7c47849f2d1eb2b/gistfile1.json")!
+        delay(2, closure: {
+            self.fetchMovies(url)
+            self.refreshControl.endRefreshing()
+        })
+
+    }
+    
     func fetchMovies(url: NSURL) -> Void {
         let request = NSURLRequest(URL: url)
         MBProgressHUD.showHUDAddedTo(self.view, animated: true)
         let sessionConfiguration = NSURLSessionConfiguration.defaultSessionConfiguration()
         let session = NSURLSession(configuration: sessionConfiguration, delegate: nil, delegateQueue: NSOperationQueue.mainQueue())
+        let tableWidth = self.tableView.frame.width
+        let tableHeight = self.tableView.frame.height
+        
         let jsonTask = session.dataTaskWithRequest(request) {(data, response, error) -> Void in
             do {
                 let json = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers)
                 self.movies = json["movies"] as? [NSDictionary]
+                if(!self.errorAlertView.hidden) {
+                    self.errorAlertView.hidden = true
+                    self.tableView.frame = CGRect(x: 0, y: self.originTableViewOriginY, width: tableWidth, height: tableHeight)
+                }
                 self.tableView.reloadData()
             } catch {
+                self.tableView.frame = CGRect(x: 0, y: self.originTableViewOriginY + 50, width: tableWidth, height: tableHeight)
                 self.errorAlertView.hidden = false
                 print("error: \(error)")
             }
